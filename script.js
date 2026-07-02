d0f37eb18b851805ddba8c353a576e86ea96f765
Fiza-Rani-057 <fahadsaeed057@gmail.com>
2026-07-02 21:08:58 +0500

    Function

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
- 
+//  Question 1
+
+ document.write( "<h1>" + "Question 1" + "<br>" + "<br>" + "</h1>");
+
+ function CurrentDate(currentDate){ 
+    var date = new Date();
+    document.write(date);
+
+ }
+ CurrentDate();
+
+//  Question 2
+
+  document.write( "<h1>" + "Question 2" + "<br>" + "<br>" + "</h1>");
+
+  function greetUser(greetUser){
+   var firstName = prompt("Enter your first name");
+   var lastName = prompt("Enter your last name");
+   document.write("Hello " + firstName +  " " + lastName );
+  }
+  greetUser();

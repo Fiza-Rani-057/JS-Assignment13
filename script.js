19bd4140f82e9ad23e9b2fa49a8c3fe28d103740
Fiza-Rani-057 <fahadsaeed057@gmail.com>
2026-07-02 21:14:19 +0500

    Functions

--- script.js
+++ script.js
@@ -19,3 +19,15 @@
    document.write("Hello " + firstName +  " " + lastName );
   }
   greetUser();
+
+//   Question 3
+
+  document.write( "<h1>" + "Question 3" + "<br>" + "<br>" + "</h1>");
+
+  function Sum(sumNumbers){
+    var firstNumber = parseFloat(prompt("Enter first Number"));
+    var secondNumber = parseInt(prompt("Enter second Number"));
+    var addNumbers = firstNumber + secondNumber;
+    document.write("The Sum of the numbers is: " + addNumbers );
+  }
+  Sum();
